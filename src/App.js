40e0dff785e50432f94aa388257500a9fc19d73c
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2021-04-04 14:01:27 -0400

    broken homepage into smaller components and finished styling our menu item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './pages/homepage.component.jsx'
 
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
