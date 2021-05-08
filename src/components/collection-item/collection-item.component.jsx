80e4ee80d36e3b80fb3b093e07d7a60b78a04022
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2021-05-08 19:42:58 -0400

    updated shop data and updated cart utils to group items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
+import {connect} from 'react-redux'
+import CustomButton from '../custom-button/custom-button.component'
+import "./collection-item.styles.scss";
+import {addItem} from '../../redux/cart/cart.actions'
 
-import './collection-item.styles.scss'
-
-const CollectionItem = ({id, name, imageUrl, price}) => (
-    <div className='collection-item'>
-        <div style = {{backgroundImage: `url(${imageUrl})`}} className='image'/>
-    <div className='collection-footer'>
-        <span className = 'name'>{name}</span>
-        <span className = 'price'>{price}</span>
+const CollectionItem = ({ item, addItem}) => (
+  <div className="collection-item">
+    <div style={{ backgroundImage: `url(${item.imageUrl})` }} className="image" />
+    <div className="collection-footer">
+      <span className="name">{item.name}</span>
+      <span className="price">{item.price}</span>
     </div>
-        </div>
-)
+    <CustomButton onClick ={()=>addItem(item)} inverted>ADD TO CART</CustomButton>
+  </div>
+);
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch =>({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,7 +6,8 @@ const CollectionPreview = ({title, items})=>(
     <div className = 'collection-preview'>
     <h1 className = 'title'>{title}</h1>
     <div className ='preview'>
-    {items.filter((ele,ind)=> ind<4).map(({...otherProps})=> <CollectionItem {...otherProps}/>)}
+    {items.filter((ele,ind)=> ind<4)
+        .map((item)=> <CollectionItem key={item.id} item={item}/>)}
     </div>
     </div>
 )
