b9347bc677319aa104b40d1f3f83391fb215b156
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2021-05-12 08:13:14 -0400

    finished checkout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -9,3 +9,14 @@ export const addItem = item =>({
     payload:item
 })
 
+export const clearItem = item =>({
+    type:CartActionTypes.CLEAR_ITEM,
+    payload:item
+})
+
+export const removeItem = item =>({
+    type:CartActionTypes.REMOVE_ITEM,
+    payload:item
+})
+
+
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,19 @@ export const addItemToCart = (cartItems, cartItemToAdd)=>{
     }
 
 }
+
+export const removeItem = (cartItems, cartItemToRemove)=>{
+    const existingCartItem = cartItems.find(cartItem=>cartItem.id===cartItemToRemove.id);
+
+    if (existingCartItem.quantity===1){
+        return cartItems.filter(cartItem=>cartItem.id!==cartItemToRemove.id)
+    }
+
+    return cartItems.map(cartItem=>
+        cartItem.id===cartItemToRemove.id ?
+        {...cartItemToRemove, quantity:cartItemToRemove.quantity-1} :
+        cartItem
+        )
+
+
+}
